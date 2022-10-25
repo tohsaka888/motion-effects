ec438404f505a83bd22d63e216ceca46b8d083be
tohsaka888 <156132264@qq.com>
2022-10-25 10:54:42 +0800

    ✨ feat: demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,55 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import { Album } from "../motion-effect";
-import styles from "../styles/Home.module.css";
+import { Album, Typing } from "../motion-effect";
 
 const Home: NextPage = () => {
   return (
-    <Album
-      text={
-        <div style={{ color: "#fff", fontSize: "1.5rem" }}>
-          {"This is a text!"}
-        </div>
-      }
-      subText={<div style={{ color: "#fff" }}>{"This is a **subText**!"}</div>}
-    >
-      <div
-        style={{
-          width: "500px",
-          height: "300px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+    <>
+      <Album
+        style={{ margin: "8px" }}
+        duration={0.4}
+        text={
+          <div style={{ color: "#fff", fontSize: "1.3rem" }}>
+            {"Umi4 正式版发布"}
+          </div>
+        }
+        tag={"推广"}
+        subText={
+          <div style={{ color: "#fff", fontSize: "8px" }}>
+            {"Umi4现在可以在npm上使用了!"}
+          </div>
+        }
       >
-        <div style={{ fontSize: "2rem" }}>Title</div>
-      </div>
-    </Album>
+        <Image
+          src="https://gw.alipayobjects.com/zos/bmw-prod/5741382d-cc22-4ede-b962-aea287a1d1a1/l4nq43o8_w2646_h1580.png"
+          alt="Umi 4 正式版发布"
+          width={650}
+          height={380}
+        />
+      </Album>
+      <Typing
+        text={"Hello, I am Tohsaka888!"}
+        speed={5}
+        conatinerStyle={{ display: "flex", alignItems: "flex-end" }}
+        textStyle={{
+          fontSize: "2rem",
+          fontWeight: "bold",
+          letterSpacing: "1px",
+        }}
+        cursor={
+          <div
+            style={{
+              fontSize: "2rem",
+              fontWeight: "bolder",
+              marginBottom: "2px",
+              marginLeft: "8px",
+            }}
+          >
+            |
+          </div>
+        }
+      />
+    </>
   );
 };
 
