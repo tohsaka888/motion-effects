83045d182019fec753f6390f2cddb5fdbff56b17
tohsaka888 <156132264@qq.com>
2022-10-25 10:53:48 +0800

    ✨ feat: type

---
+++ motion-effect/type.ts
@@ -0,0 +1,18 @@
+import { HTMLMotionProps } from "framer-motion";
+import React, { CSSProperties } from "react";
+
+export type AlbumProps = {
+  children: React.ReactNode;
+  text: React.ReactNode;
+  subText: React.ReactNode;
+  tag?: string;
+  duration?: number;
+} & HTMLMotionProps<"div">;
+
+export type TypingProps = {
+  text: string;
+  speed: number;
+  conatinerStyle?: CSSProperties;
+  textStyle?: CSSProperties;
+  cursor?: React.ReactNode;
+};
