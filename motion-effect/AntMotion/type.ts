381f8847a6937d0c274b5047a57899e5d10d97b4
tohsaka888 <156132264@qq.com>
2022-11-05 17:10:27 +0800

    ✨ feat(type): AntMotion type

---
+++ motion-effect/AntMotion/type.ts
@@ -0,0 +1,11 @@
+export type SwitchConfigProps = {
+  pic?: string;
+  color: string;
+  background: string;
+};
+
+export type DetailSwitchProps = {
+  width: string;
+  height: string;
+  config: SwitchConfigProps[];
+};
